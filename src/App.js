8d32d954681e8116c31b9f0f7e27d550bf60d104
Max <montrealist@users.noreply.github.com>
2020-04-01 12:47:02 -0400

    added tachyons css lib

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="tc pv4 pv5-ns">
+                <h1 className="f5 f4-ns fw6 mid-gray">Tip calculator</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
