cf2eceee88392d12a5c339c40d6dd3c573d72fc4
Max <montrealist@users.noreply.github.com>
2020-04-01 12:54:43 -0400

    created dummy button component to test deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Button from "./components/Button";
 
 function App() {
+    const submit = e => {
+        e.preventDefault();
+        console.log("calculatin...");
+    };
     return (
         <div className="App">
             <header className="tc pv4 pv5-ns">
                 <h1 className="f5 f4-ns fw6 mid-gray">Tip calculator</h1>
             </header>
+            <Button onClick={submit}>Calculate</Button>
         </div>
     );
 }
